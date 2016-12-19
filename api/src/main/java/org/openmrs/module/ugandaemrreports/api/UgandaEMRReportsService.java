af0e9c64e0ac7b7be3f4bfb7468307c645674971
Charles Olupot <olupotcharles@gmail.com>
2016-12-19 13:30:45 +0300

    Initial setup of UgandaEMR Reports to use OpenMRS 2.x

--- api/src/main/java/org/openmrs/module/ugandaemrreports/api/UgandaEMRReportsService.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/api/UgandaEMRReportsService.java
@@ -1,20 +1,48 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.ugandaemrreports.api;
 
+import org.openmrs.annotation.Authorized;
+import org.openmrs.api.APIException;
 import org.openmrs.api.OpenmrsService;
+import org.openmrs.module.ugandaemrreports.UgandaEMRReportsConfig;
+import org.openmrs.module.ugandaemrreports.Item;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * This service exposes module's core functionality. It is a Spring managed bean which is configured in
- * moduleApplicationContext.xml.
- * <p>
- * It can be accessed only via Context:<br>
- * <code>
- * Context.getService(UgandaEMRReportsService.class).someMethod();
- * </code>
- *
- * @see org.openmrs.api.context.Context
+ * The main service of this module, which is exposed for other modules. See
+ * moduleApplicationContext.xml on how it is wired up.
  */
-@Transactional
 public interface UgandaEMRReportsService extends OpenmrsService {
 	
+	/**
+	 * Returns an item by uuid. It can be called by any authenticated user. It is fetched in read
+	 * only transaction.
+	 * 
+	 * @param uuid
+	 * @return
+	 * @throws APIException
+	 */
+	@Authorized()
+	@Transactional(readOnly = true)
+	Item getItemByUuid(String uuid) throws APIException;
+	
+	/**
+	 * Saves an item. Sets the owner to superuser, if it is not set. It can be called by users with
+	 * this module's privilege. It is executed in a transaction.
+	 * 
+	 * @param item
+	 * @return
+	 * @throws APIException
+	 */
+	@Authorized(UgandaEMRReportsConfig.MODULE_PRIVILEGE)
+	@Transactional
+	Item saveItem(Item item) throws APIException;
 }
--- api/src/main/java/org/openmrs/module/ugandaemrreports/definition/cohort/definition/MissedAppointmentCohortDefinition.java
+++
@@ -1,9 +0,0 @@
-package org.openmrs.module.ugandaemrreports.definition.cohort.definition;
-
-import org.openmrs.module.reporting.cohort.definition.BaseCohortDefinition;
-
-/**
- * Created by carapai on 19/09/2016.
- */
-public class MissedAppointmentCohortDefinition extends BaseCohortDefinition {
-}
