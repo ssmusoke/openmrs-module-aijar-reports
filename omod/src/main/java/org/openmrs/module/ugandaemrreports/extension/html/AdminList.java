dba90a8327d235a64c89422bc45478dd51baf48c
Charles Olupot <olupotcharles@gmail.com>
2017-01-04 16:28:21 +0300

    Initial setup of UgandaEMR Reports to use OpenMRS 2.x

--- omod/src/main/java/org/openmrs/module/ugandaemrreports/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/ugandaemrreports/extension/html/AdminList.java
@@ -17,7 +17,8 @@ import org.openmrs.module.web.extension.AdministrationSectionExt;
 
 /**
  * This class defines the links that will appear on the administration page under the
- * "ugandaemrreports.title" heading. This extension is enabled by defining (uncommenting) it in the config.xml file.
+ * "ugandaemrreports.title" heading. This extension is enabled by defining (uncommenting) it in the
+ * config.xml file.
  */
 public class AdminList extends AdministrationSectionExt {
 	
@@ -44,7 +45,7 @@ public class AdminList extends AdministrationSectionExt {
 		
 		map.put("module/ugandaemrreports/ugandaemrreports.form", "ugandaemrreports.title");
 		map.put("ugandaemrreports/ugandaemrreports.page", "ugandaemrreports.refapp.title");
-
+		
 		return map;
 	}
 	
