0a113455a31649d2b5154acd65fe6b3dc9aa3d9a
Ken Omondi <ken@Linux65.(none)>
2017-05-24 18:39:05 +0300

    Initial OPD Register Implementation
    Routine fixes

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupOPDRegister.java
@@ -0,0 +1,100 @@
+package org.openmrs.module.ugandaemrreports.reports;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.ugandaemrreports.definition.dataset.definition.OPDDatasetDefinition;
+import org.openmrs.module.ugandaemrreports.library.DataFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * OPD Register
+ */
+@Component
+public class SetupOPDRegister extends UgandaEMRDataExportManager {
+
+    @Autowired
+    private DataFactory df;
+
+    /**
+     * @return the uuid for the report design for exporting to Excel
+     */
+    @Override
+    public String getExcelDesignUuid() {
+        return "6f935ab6-3407-4321-ae00-26c75ca2166f";
+    }
+
+    @Override
+    public String getUuid() {
+        return "df041e1d-0043-4a01-815b-c9f66edcac7e";
+    }
+
+    @Override
+    public String getName() {
+        return "OPD Register";
+    }
+
+    @Override
+    public String getDescription() {
+        return "OPD Register";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(df.getStartDateParameter());
+        return l;
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        List<ReportDesign> l = new ArrayList<ReportDesign>();
+        l.add(buildReportDesign(reportDefinition));
+        return l;
+    }
+
+    /**
+     * Build the report design for the specified report, this allows a user to override the report design by adding
+     * properties and other metadata to the report design
+     *
+     * @param reportDefinition
+     * @return The report design
+     */
+    @Override
+
+    public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
+        ReportDesign rd = createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "031-OutPatientRegister.xls");
+        Properties props = new Properties();
+        props.put("repeatingSections", "sheet:1,row:7,dataset:OPD");
+        props.put("sortWeight", "5000");
+        rd.setProperties(props);
+        return rd;
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+
+        ReportDefinition rd = new ReportDefinition();
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+
+        OPDDatasetDefinition dsd = new OPDDatasetDefinition();
+        dsd.setName(getName());
+        dsd.setParameters(getParameters());
+        rd.addDataSetDefinition("OPD", Mapped.mapStraightThrough(dsd));
+        return rd;
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.1";
+    }
+}
