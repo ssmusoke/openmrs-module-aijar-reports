01c226abe9e860672558ba09dc09732619009c6f
ningosi <nicotwendelee@gmail.com>
2017-05-30 10:55:59 +0300

    Clean up and template adjustment

--- api/src/main/java/org/openmrs/module/ugandaemrreports/data/converter/IptCtxConverter.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/data/converter/IptCtxConverter.java
@@ -15,7 +15,6 @@ package org.openmrs.module.ugandaemrreports.data.converter;
 
 import org.openmrs.Concept;
 import org.openmrs.Obs;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.ugandaemrreports.reporting.metadata.Dictionary;
 
@@ -33,22 +32,22 @@ public class IptCtxConverter implements DataConverter {
         //get the coded value for the results
         Concept value = ((Obs)obj).getValueCoded();
 
-        if(value.equals(Dictionary.getConcept("0192ca59-b647-4f88-b07e-8fda991ba6d6"))){
+        if(value != null && value.equals(Dictionary.getConcept("0192ca59-b647-4f88-b07e-8fda991ba6d6"))){
             return "1";
         }
-        else if(value.equals(Dictionary.getConcept("f1d5afce-8dfe-4d2d-b24b-051815d61848"))){
+        else if(value != null && value.equals(Dictionary.getConcept("f1d5afce-8dfe-4d2d-b24b-051815d61848"))){
             return "2";
         }
-        else if(value.equals(Dictionary.getConcept("f29f43c6-076a-4f70-9ae8-4563ac3fda80"))){
+        else if(value != null && value.equals(Dictionary.getConcept("f29f43c6-076a-4f70-9ae8-4563ac3fda80"))){
             return "ND";
         }
-        else if(value.equals(Dictionary.getConcept("dca06bae-30ab-102d-86b0-7a5022ba4115"))){
+        else if(value != null && value.equals(Dictionary.getConcept("dca06bae-30ab-102d-86b0-7a5022ba4115"))){
             return "C";
         }
-        else if(value.equals(Dictionary.getConcept("fca28768-50dc-4d6b-a3d2-2aae3b376b27"))){
+        else if(value != null && value.equals(Dictionary.getConcept("fca28768-50dc-4d6b-a3d2-2aae3b376b27"))){
             return "CTX";
         }
-        else if(value.equals(Dictionary.getConcept("b0439b8e-469e-43e6-9dda-9b6b49f2147b"))){
+        else if(value != null && value.equals(Dictionary.getConcept("b0439b8e-469e-43e6-9dda-9b6b49f2147b"))){
             return "CTX✔";
         }
 
