8532738e85eacf633fb70591ca974864bfde08ee
Kenneth Omondi Ochieng <omondi.ken@gmail.com>
2017-06-01 11:56:45 +0300

    WIP:HMIS072-Maternity-Register (#35)
     Added WIP:HMIS072-Maternity-Register

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/definition/dataset/definition/MaternityDatasetDefinition.java
@@ -0,0 +1,31 @@
+package org.openmrs.module.ugandaemrreports.definition.dataset.definition;
+
+import java.util.Date;
+
+import org.openmrs.module.reporting.dataset.definition.BaseDataSetDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+
+public class MaternityDatasetDefinition extends BaseDataSetDefinition {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@ConfigurationProperty
+    private Date startDate;
+
+    public MaternityDatasetDefinition() {
+        super();
+    }
+
+    public MaternityDatasetDefinition(String name, String description) {
+        super(name, description);
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }	
+}
