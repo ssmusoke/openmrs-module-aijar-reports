259fc8a0def6e49954b1a7b377233a0e8f383d68
ningosi <nicotwendelee@gmail.com>
2017-07-11 16:07:36 +0300

    Initial work on SMC register

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/data/converter/FaciltyAndOutReachDataConverter.java
@@ -0,0 +1,54 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.ugandaemrreports.data.converter;
+
+import org.openmrs.Concept;
+import org.openmrs.Obs;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+import org.openmrs.module.ugandaemrreports.reporting.metadata.Dictionary;
+
+/**
+ * Created by Nicholas Ingosi on 5/22/17.
+ */
+public class FaciltyAndOutReachDataConverter implements DataConverter {
+    @Override
+    public Object convert(Object obj) {
+
+        if (obj == null) {
+            return "";
+        }
+        Concept concept = ((Obs) obj).getValueCoded();
+        if(concept != null && concept.equals(Dictionary.getConcept("f2aa1852-fcfe-484b-a6ef-1613bd3a1a7f"))){
+            return "F";
+        }
+        else if(concept != null && concept.equals(Dictionary.getConcept("03596df2-09bc-4d1f-94fd-484411ac9012"))){
+            return "OR";
+        }
+        else if(concept != null && concept.equals(Dictionary.getConcept("63e5387f-74f6-4a92-a71f-7b5dd3ed8432"))){
+            return "CAMP";
+        }
+
+        return null;
+    }
+
+    @Override
+    public Class<?> getInputDataType() {
+        return Obs.class;
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return String.class;
+    }
+}
---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reporting/calculation/smc/AgeFromEncounterDateCalculation.java
@@ -0,0 +1,86 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.ugandaemrreports.reporting.calculation.smc;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.calculation.patient.PatientCalculationContext;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.calculation.result.SimpleResult;
+import org.openmrs.module.ugandaemrreports.reporting.calculation.AbstractPatientCalculation;
+import org.openmrs.module.ugandaemrreports.reporting.calculation.EmrCalculationUtils;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Created by Nicholas Ingosi on 5/19/17.
+ */
+public class AgeFromEncounterDateCalculation extends AbstractPatientCalculation {
+    @Override
+    public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> params, PatientCalculationContext context) {
+        CalculationResultMap ret = new CalculationResultMap();
+        Integer lower = (params != null && params.containsKey("lower")) ? (Integer) params.get("lower") : null;
+        Integer upper = (params != null && params.containsKey("upper")) ? (Integer) params.get("upper") : null;
+
+        CalculationResultMap encounterDate = calculate(new SMCEncounterDateCalculation(), cohort, context);
+        for(Integer ptId:cohort) {
+            Date dateAtRegistration;
+            Integer ageAtRegistration = null;
+            Integer age = null;
+            Date birthDate = Context.getPersonService().getPerson(ptId).getBirthdate();
+            if(encounterDate != null) {
+                dateAtRegistration = EmrCalculationUtils.datetimeResultForPatient(encounterDate, ptId);
+                if(dateAtRegistration != null && birthDate != null){
+                    ageAtRegistration = ageInYears(birthDate, dateAtRegistration);
+                    if(ageAtRegistration != null && ageAtRegistration < 1) {
+                        age = ageInMonths(birthDate, dateAtRegistration);
+                    }
+                    else if(ageAtRegistration != null && lower != null && lower >= 49 && ageAtRegistration >= 49){
+                        age = ageAtRegistration;
+                    }
+                    else if(lower != null && upper != null & ageAtRegistration != null && ageAtRegistration >= lower && ageAtRegistration <= upper) {
+                        age = ageAtRegistration;
+                    }
+                }
+            }
+            ret.put(ptId, new SimpleResult(age, this));
+        }
+        return ret;
+    }
+
+    private Integer ageInYears(Date dob, Date encounterDate){
+        Integer age;
+        Calendar calDOB = getCalendar(dob);
+        Calendar calEncounterDate = getCalendar(encounterDate);
+        age = calEncounterDate.get(Calendar.YEAR) - calDOB.get(Calendar.YEAR);
+        return age;
+    }
+
+    private Integer ageInMonths(Date dob, Date encounterDate){
+        Integer age;
+        Calendar calDOB = getCalendar(dob);
+        Calendar calEncounterDate = getCalendar(encounterDate);
+        age = calEncounterDate.get(Calendar.MONTH) - calDOB.get(Calendar.MONTH);
+        return age;
+    }
+
+
+    private Calendar getCalendar(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
+}
