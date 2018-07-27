dc5ea9bc38e9cb60cb9076cab27313ddfacc31ed
Stephen S Musoke <ssmusoke@gmail.com>
2018-07-27 14:30:13 +0300

    completed PMTCT STAT report with alll values entered

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupPMTCTSTATReport.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupPMTCTSTATReport.java
@@ -12,6 +12,7 @@ import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.openmrs.module.ugandaemrreports.library.*;
 import org.openmrs.module.ugandaemrreports.metadata.HIVMetadata;
+import org.openmrs.module.ugandaemrreports.reporting.utils.ReportUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -52,6 +53,9 @@ public class SetupPMTCTSTATReport extends UgandaEMRDataExportManager {
     @Autowired
     private CommonDimensionLibrary commonDimensionLibrary;
 
+    @Autowired
+    private Moh105IndicatorLibrary indicatorLibrary;
+
 
     /**
      * @return the uuid for the report design for exporting to Excel
@@ -85,6 +89,7 @@ public class SetupPMTCTSTATReport extends UgandaEMRDataExportManager {
     }
 
 
+
     @Override
     public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
         return Arrays.asList(buildReportDesign(reportDefinition));
@@ -104,6 +109,7 @@ public class SetupPMTCTSTATReport extends UgandaEMRDataExportManager {
 
     @Override
     public ReportDefinition constructReportDefinition() {
+        String params = "startDate=${startDate},endDate=${endDate}";
 
         ReportDefinition rd = new ReportDefinition();
 
@@ -121,172 +127,54 @@ public class SetupPMTCTSTATReport extends UgandaEMRDataExportManager {
         CohortDefinitionDimension ageDimension =commonDimensionLibrary.getPMTCT_STAT_AgeGenderGroup();
         dsd.addDimension("age", Mapped.mapStraightThrough(ageDimension));
 
-        CohortDefinition enrolledOnOrBeforeQuarter = hivCohortDefinitionLibrary.getEnrolledInCareByEndOfPreviousDate();
-        CohortDefinition enrolledInTheQuarter = hivCohortDefinitionLibrary.getEnrolledInCareBetweenDates();
+       addGender(dsd,"a","Total  Pregnant With known HIV status at entry",ReportUtils.map(indicatorLibrary.pregnantTrkTrrk(),params),"female"); ;
+        addGender(dsd,"b","Total  Pregnant Known HIV positives at entry",ReportUtils.map(indicatorLibrary.pregnantTrrk(),params),"female");
+        addGender(dsd,"c","Pregnant Total Newly tested for HIV",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRAndTRR(),params),"female");
+        addGender(dsd,"d","Total  newly Pregnant Tested  HIV Positive",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRR(),params),"female");
+        addGender(dsd,"e","Total  Number of NEW ANC Clients ",ReportUtils.map(indicatorLibrary.anc1stVisit()),"female");
+        addGender(dsd,"f","Total  Male that tested For HIV",ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params),"male");
+        addGender(dsd,"g","Total  Male that tested HIV +ve",ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultHivPositive(), params),"male");
 
-        // CohortDefinition activeWithNoEncounterInQuarter = hivCohortDefinitionLibrary.getActiveWithNoEncounterInQuarter();
-
-        CohortDefinition hadEncounterInQuarter = hivCohortDefinitionLibrary.getArtPatientsWithEncounterOrSummaryPagesBetweenDates();
-
-        CohortDefinition transferredInTheQuarter = hivCohortDefinitionLibrary.getTransferredInToCareDuringPeriod();
-        CohortDefinition transferredInBeforeQuarter = hivCohortDefinitionLibrary.getTransferredInToCareBeforePeriod();
-
-        CohortDefinition onArtDuringQuarter = hivCohortDefinitionLibrary.getPatientsHavingRegimenDuringPeriod();
-        CohortDefinition onArtBeforeQuarter = hivCohortDefinitionLibrary.getPatientsHavingRegimenBeforePeriod();
-
-        CohortDefinition havingBaseRegimenDuringQuarter = hivCohortDefinitionLibrary.getPatientsHavingBaseRegimenDuringPeriod();
-        CohortDefinition havingBaseRegimenBeforeQuarter = hivCohortDefinitionLibrary.getPatientsHavingBaseRegimenBeforePeriod();
-
-        CohortDefinition havingArtStartDateDuringQuarter = hivCohortDefinitionLibrary.getArtStartDateBetweenPeriod();
-        CohortDefinition havingArtStartDateBeforeQuarter = hivCohortDefinitionLibrary.getArtStartDateBeforePeriod();
-
-        CohortDefinition enrolledWhenPregnantOrLactating = hivCohortDefinitionLibrary.getEnrolledInCareToCareWhenPregnantOrLactating();
-
-        CohortDefinition pregnantAtFirstEncounter = hivCohortDefinitionLibrary.getPatientsPregnantAtFirstEncounter();
-
-        CohortDefinition onINHDuringQuarter = hivCohortDefinitionLibrary.getOnINHDuringPeriod();
-        CohortDefinition onINHBeforeQuarter = hivCohortDefinitionLibrary.getOnINHDuringBeforePeriod();
-
-        CohortDefinition onCPTDuringQuarter = hivCohortDefinitionLibrary.getOnCPTDuringPeriod();
-
-        CohortDefinition assessedForTBDuringQuarter = hivCohortDefinitionLibrary.getAccessedForTBDuringPeriod();
-
-        CohortDefinition diagnosedWithTBDuringQuarter = hivCohortDefinitionLibrary.getDiagnosedWithTBDuringPeriod();
-
-        CohortDefinition onTBRxDuringQuarter = hivCohortDefinitionLibrary.getStartedTBRxDuringPeriod();
-        CohortDefinition onTBRxBeforeQuarter = hivCohortDefinitionLibrary.getStartedTBRxBeforePeriod();
-
-        CohortDefinition oedemaWasTakenDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoseOedemaWasTakenDuringPeriod();
-
-        CohortDefinition mUACWasTakenDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoMUACWasTakenDuringPeriod();
-
-        CohortDefinition assessedForMalnutritionDuringQuarter = hivCohortDefinitionLibrary.getPatientsAssessedForMalnutrition();
-
-        CohortDefinition heightWasTakenDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoHeightWasTakenDuringPeriod();
-
-        CohortDefinition weightWasTakenDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoWeightWasTakenDuringPeriod();
-
-        CohortDefinition baseWeightWasTakenDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoBaseWeightWasTakenDuringPeriod();
-
-        CohortDefinition mUACWasYellowOrRedDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoMUACWasRedOrYellowDuringPeriod();
-
-        CohortDefinition malnourishedDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoAreMalnourishedDuringPeriod();
-
-        CohortDefinition oedemaWasYesDuringQuarter = hivCohortDefinitionLibrary.getPatientsWhoseOedemaWasYesDuringPeriod();
-
-        CohortDefinition onArtBasedOnCD4 = hivCohortDefinitionLibrary.getPatientsStartedArtBasedOnCD4();
-
-        CohortDefinition eligibleByEndOfQuarter = hivCohortDefinitionLibrary.getEligibleAndReadyByEndOfQuarter();
-
-        CohortDefinition childrenOnFirstLineDuringQuarter = df.getPatientsInAll(commonCohortDefinitionLibrary.MoHChildren(), hivCohortDefinitionLibrary.getChildrenOnFirstLineRegimenDuringPeriod());
-        CohortDefinition childrenOnSecondLineDuringQuarter = df.getPatientsInAll(commonCohortDefinitionLibrary.MoHChildren(), hivCohortDefinitionLibrary.getChildrenOnSecondLineRegimenDuringPeriod());
-
-        CohortDefinition adultsOnFirstLineDuringQuarter = df.getPatientsInAll(commonCohortDefinitionLibrary.MoHAdult(), hivCohortDefinitionLibrary.getAdultsOnFirstLineRegimenDuringPeriod());
-        CohortDefinition adultsOnSecondLineDuringQuarter = df.getPatientsInAll(commonCohortDefinitionLibrary.MoHAdult(), hivCohortDefinitionLibrary.getAdultsOnSecondLineRegimenDuringPeriod());
-
-        CohortDefinition onThirdLineRegimenDuringQuarter = hivCohortDefinitionLibrary.getPatientsOnThirdLineRegimenDuringPeriod();
-
-        CohortDefinition patientsWithGoodAdherenceDuringQuarter = hivCohortDefinitionLibrary.getPatientsWithGoodAdherence();
-
-        CohortDefinition beenOnArtBeforeQuarter = df.getPatientsInAny(onArtBeforeQuarter, havingArtStartDateBeforeQuarter, havingBaseRegimenBeforeQuarter);
-        CohortDefinition beenOnArtDuringQuarter = df.getPatientsInAny(onArtDuringQuarter, havingArtStartDateDuringQuarter, havingBaseRegimenDuringQuarter);
-
-        CohortDefinition everEnrolledByEndQuarter = df.getPatientsNotIn(enrolledOnOrBeforeQuarter, enrolledInTheQuarter);
-        CohortDefinition enrolledDuringTheQuarter = df.getPatientsNotIn(enrolledInTheQuarter, transferredInTheQuarter);
-
-        CohortDefinition startedINHDuringQuarter = df.getPatientsNotIn(onINHDuringQuarter, onINHBeforeQuarter);
-
-        CohortDefinition cumulativeEverEnrolled = df.getPatientsInAny(everEnrolledByEndQuarter, enrolledDuringTheQuarter);
-
-        CohortDefinition onPreArt = df.getPatientsNotIn(hadEncounterInQuarter, df.getPatientsInAny(beenOnArtBeforeQuarter, beenOnArtDuringQuarter));
-
-        CohortDefinition onPreArtWhoReceivedCPT = df.getPatientsInAll(onPreArt, onCPTDuringQuarter);
-
-        CohortDefinition onPreArtAssessedForTB = df.getPatientsInAll(onPreArt, assessedForTBDuringQuarter);
-
-        CohortDefinition onPreArtDiagnosedWithTB = df.getPatientsInAll(onPreArt, diagnosedWithTBDuringQuarter);
-
-        CohortDefinition startedTBDuringQuarter = df.getPatientsNotIn(onTBRxDuringQuarter, onTBRxBeforeQuarter);
-
-        CohortDefinition onPreArtStartedTBRx = df.getPatientsInAll(onPreArt, startedTBDuringQuarter);
-
-        CohortDefinition assessedForMalnutrition = df.getPatientsInAny(oedemaWasTakenDuringQuarter, mUACWasTakenDuringQuarter, assessedForMalnutritionDuringQuarter, heightWasTakenDuringQuarter, weightWasTakenDuringQuarter, baseWeightWasTakenDuringQuarter);
-
-        CohortDefinition onPreArtAssessedForMalnutrition = df.getPatientsInAll(onPreArt, assessedForMalnutrition);
-
-        CohortDefinition whoAreMalnourished = df.getPatientsInAny(mUACWasYellowOrRedDuringQuarter, malnourishedDuringQuarter, oedemaWasYesDuringQuarter);
-        CohortDefinition onPreArtWhoAreMalnourished = df.getPatientsInAll(onPreArt, whoAreMalnourished);
-
-        CohortDefinition startedBasedOnCD4 = df.getPatientsInAll(havingArtStartDateDuringQuarter, onArtBasedOnCD4);
-
-        CohortDefinition cumulativeOnArt = df.getPatientsInAny(havingArtStartDateBeforeQuarter, havingArtStartDateDuringQuarter);
-
-        CohortDefinition onFirstLineRegimen = df.getPatientsInAll(beenOnArtDuringQuarter, df.getPatientsInAny(childrenOnFirstLineDuringQuarter, adultsOnFirstLineDuringQuarter));
-        CohortDefinition onSecondLineRegimen = df.getPatientsInAll(beenOnArtDuringQuarter, df.getPatientsInAny(childrenOnSecondLineDuringQuarter, adultsOnSecondLineDuringQuarter));
-
-        CohortDefinition activeOnArtOnCPT = df.getPatientsInAll(beenOnArtDuringQuarter, onCPTDuringQuarter);
-        CohortDefinition activeOnArtAssessedForTB = df.getPatientsInAll(beenOnArtDuringQuarter, assessedForTBDuringQuarter);
-        CohortDefinition activeOnArtDiagnosedWithTB = df.getPatientsInAll(beenOnArtDuringQuarter, diagnosedWithTBDuringQuarter);
-        CohortDefinition activeOnArtStartedTBRx = df.getPatientsInAll(beenOnArtDuringQuarter, startedTBDuringQuarter);
-        CohortDefinition activeOnArtOnTBRx = df.getPatientsInAll(beenOnArtDuringQuarter, onTBRxDuringQuarter);
-        CohortDefinition activeOnArtWithGoodAdherence = df.getPatientsInAll(beenOnArtDuringQuarter, patientsWithGoodAdherenceDuringQuarter);
-
-        CohortDefinition activeOnArtAssessedForMalnutrition = df.getPatientsInAll(beenOnArtDuringQuarter, assessedForMalnutrition);
-
-        CohortDefinition activeOnArtWhoAreMalnourished = df.getPatientsInAll(beenOnArtDuringQuarter, whoAreMalnourished);
-
-        CohortDefinition eligibleButNotStartedByQuarter = df.getPatientsNotIn(eligibleByEndOfQuarter, cumulativeOnArt);
-
-        CohortDefinition startedArtWhenPregnant = df.getPatientsInAll(pregnantAtFirstEncounter, havingArtStartDateDuringQuarter);
-
-        addGender(dsd,"a","Total  Pregnant With HIV status at entry",beenOnArtBeforeQuarter,"female");
-        addGender(dsd,"b","Total  Pregnant Known HIV positives at entry",beenOnArtBeforeQuarter,"female");
-        addGender(dsd,"c","Total  Pregnant Total Newly tested for HIV",beenOnArtBeforeQuarter,"female");
-        addGender(dsd,"d","Total  Pregnant Tested  HIV Positive",beenOnArtBeforeQuarter,"female");
-        addGender(dsd,"e","Total  Number of NEW ANC Clients ",beenOnArtBeforeQuarter,"female");
-       // addGender(dsd,"a","Total  Pregnant With HIV status at entry",beenOnArtBeforeQuarter,"female");
-        addGender(dsd,"f","Total  Male that tested For HIV",beenOnArtBeforeQuarter,"male");
-        addGender(dsd,"g","Total  Male that tested HIV +ve",beenOnArtBeforeQuarter,"male");
-        add105AgeGenderDimensions(dsd,"h","Pregnant newly and tested for HIV",beenOnArtBeforeQuarter);
-        add105AgeGenderDimensions(dsd,"i","Pregnant newly and tested HIV +ve for 1st time",beenOnArtBeforeQuarter);
-        add105AgeGenderDimensions(dsd,"j","1st ANC Visit",beenOnArtBeforeQuarter);
-
-        addIndicator(dsd, "4h" , "Total pregnant who knew HIV status before 1st ANC" , activeOnArtOnCPT, "age=female");
-        addIndicator(dsd, "5h" , "Total pregnant who were HIV +ve before 1st ANC" , activeOnArtOnCPT, "age=female");
-        addIndicator(dsd, "9h" ,"Pregnant Women testing HIV+ on a retest " , activeOnArtOnCPT, "age=female");
-        addIndicator(dsd, "13h" ,"Total Males received HIV test results" , activeOnArtOnCPT, "age=male");
-        addIndicator(dsd, "14h" ,"Males received HIV test +ve " , activeOnArtOnCPT, "age=male");
+        add105AgeGenderDimensions(dsd,"h","Pregnant newly and tested for HIV",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRAndTRR(), params));
+        add105AgeGenderDimensions(dsd,"i","Pregnant newly and tested HIV +ve for 1st time",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRR(), params));
+        add105AgeGenderDimensions(dsd,"j","1st ANC Visit",ReportUtils.map(indicatorLibrary.anc1stVisit(), params));
+//
+        addIndicator(dsd, "4h" , "Total pregnant who knew HIV status before 1st ANC" , ReportUtils.map(indicatorLibrary.pregnantTrkTrrk(), params), "");
+        addIndicator(dsd, "5h" , "Total pregnant who were HIV +ve before 1st ANC" , ReportUtils.map(indicatorLibrary.pregnantTrrk(), params), "");
+        addIndicator(dsd, "9h" ,"Pregnant Women testing HIV+ on a retest " , ReportUtils.map(indicatorLibrary.retestedTrrPlus(), params),"");
+        addIndicator(dsd, "13h" ,"Total Males received HIV test results" , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params),"");
+        addIndicator(dsd, "14h" ,"Males received HIV test +ve " , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultHivPositive(), params),"");
 
         return rd;
     }
 
 
-    public void addGender (CohortIndicatorDataSetDefinition dsd,String key ,String label,CohortDefinition cohortDefinition , String gender){
-        addIndicator(dsd, "1" + key, label , cohortDefinition, "age=below10"+gender);
-        addIndicator(dsd, "2" + key, label , cohortDefinition, "age=between10and14"+gender);
-        addIndicator(dsd, "3" + key, label , cohortDefinition, "age=between15and19"+gender);
-        addIndicator(dsd, "4" + key, label , cohortDefinition, "age=between20and24"+gender);
-        addIndicator(dsd, "5" + key, label , cohortDefinition, "age=between25and29"+gender);
-        addIndicator(dsd, "6" + key, label , cohortDefinition, "age=between30and34"+gender);
-        addIndicator(dsd, "7" + key, label , cohortDefinition, "age=between35and39"+gender);
-        addIndicator(dsd, "8" + key, label , cohortDefinition, "age=between40and49"+gender);
-        addIndicator(dsd, "9" + key, label , cohortDefinition, "age=above50"+gender);
+    public void addGender (CohortIndicatorDataSetDefinition dsd,String key ,String label,Mapped<? extends CohortIndicator> cohortIndicator , String gender){
+        addIndicator(dsd, "1" + key, label , cohortIndicator, "age=below10"+gender);
+        addIndicator(dsd, "2" + key, label , cohortIndicator, "age=between10and14"+gender);
+        addIndicator(dsd, "3" + key, label , cohortIndicator, "age=between15and19"+gender);
+        addIndicator(dsd, "4" + key, label , cohortIndicator, "age=between20and24"+gender);
+        addIndicator(dsd, "5" + key, label , cohortIndicator, "age=between25and29"+gender);
+        addIndicator(dsd, "6" + key, label , cohortIndicator, "age=between30and34"+gender);
+        addIndicator(dsd, "7" + key, label , cohortIndicator, "age=between35and39"+gender);
+        addIndicator(dsd, "8" + key, label , cohortIndicator, "age=between40and49"+gender);
+        addIndicator(dsd, "9" + key, label , cohortIndicator, "age=above50"+gender);
     }
 
-    public void add105AgeGenderDimensions(CohortIndicatorDataSetDefinition dsd,String key ,String label,CohortDefinition cohortDefinition){
-        addIndicator(dsd, "1" + key, label , cohortDefinition, "age=between10and19female");
-        addIndicator(dsd, "2" + key, label , cohortDefinition, "age=between20and24female");
-        addIndicator(dsd, "3" + key, label , cohortDefinition, "age=25+female");
+    public void add105AgeGenderDimensions(CohortIndicatorDataSetDefinition dsd,String key ,String label,Mapped<? extends CohortIndicator> cohortIndicator){
+        addIndicator(dsd, "1" + key, label , cohortIndicator, "age=between10and19female");
+        addIndicator(dsd, "2" + key, label , cohortIndicator, "age=between20and24female");
+        addIndicator(dsd, "3" + key, label , cohortIndicator, "age=25+female");
     }
 
 
-    public void addIndicator(CohortIndicatorDataSetDefinition dsd, String key, String label, CohortDefinition cohortDefinition, String dimensionOptions) {
-        CohortIndicator ci = new CohortIndicator();
-        ci.addParameter(ReportingConstants.START_DATE_PARAMETER);
-        ci.addParameter(ReportingConstants.END_DATE_PARAMETER);
-        ci.setType(CohortIndicator.IndicatorType.COUNT);
-        ci.setCohortDefinition(Mapped.mapStraightThrough(cohortDefinition));
-        dsd.addColumn(key, label, Mapped.mapStraightThrough(ci), dimensionOptions);
+    public void addIndicator(CohortIndicatorDataSetDefinition dsd, String key, String label, Mapped<? extends CohortIndicator> ci, String dimensionOptions) {
+        //CohortIndicator ci = new CohortIndicator();
+//        ci.(ReportingConstants.START_DATE_PARAMETER);
+//        ci.addParameter(ReportingConstants.END_DATE_PARAMETER);
+//        ci.setType(CohortIndicator.IndicatorType.COUNT);
+      //  ci.setCohortDefinition(Mapped.mapStraightThrough(cohortDefinition));
+        dsd.addColumn(key, label, ci, dimensionOptions);
     }
 
 
