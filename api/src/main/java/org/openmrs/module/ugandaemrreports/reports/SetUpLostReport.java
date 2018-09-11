e385d526c856baf36179bcf96f471ed54c821b74
solemabrothers <ssevvume1994solema@gmail.com>
2018-09-11 12:04:54 +0300

    Report for Lost Clients Under Facility Reports

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetUpLostReport.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetUpLostReport.java
@@ -1,68 +1,77 @@
 package org.openmrs.module.ugandaemrreports.reports;
 
-
 import org.openmrs.module.reporting.cohort.definition.BaseObsCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
 import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
-import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.openmrs.module.ugandaemrreports.definition.data.converter.BirthDateConverter;
-import org.openmrs.module.ugandaemrreports.definition.dataset.definition.EarlyWarningIndicatorsDatasetDefinition;
-import org.openmrs.module.ugandaemrreports.definition.dataset.definition.NameOfHealthUnitDatasetDefinition;
-import org.openmrs.module.ugandaemrreports.library.*;
+import org.openmrs.module.ugandaemrreports.library.ARTClinicCohortDefinitionLibrary;
+import org.openmrs.module.ugandaemrreports.library.BasePatientDataLibrary;
+import org.openmrs.module.ugandaemrreports.library.DataFactory;
+import org.openmrs.module.ugandaemrreports.library.HIVPatientDataLibrary;
 import org.openmrs.module.ugandaemrreports.metadata.HIVMetadata;
-import org.openmrs.module.ugandaemrreports.reporting.library.cohort.ARTCohortLibrary;
-import org.openmrs.reporting.data.DatasetDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+
+/**
+ * Daily Appointments List report
+ */
 @Component
 public class SetUpLostReport extends UgandaEMRDataExportManager {
+
         @Autowired
         private DataFactory df;
+
         @Autowired
         ARTClinicCohortDefinitionLibrary hivCohorts;
+
         @Autowired
         private BuiltInPatientDataLibrary builtInPatientData;
+
         @Autowired
         private HIVPatientDataLibrary hivPatientData;
+
         @Autowired
         private BasePatientDataLibrary basePatientData;
+
         @Autowired
         private HIVMetadata hivMetadata;
-        @Autowired
-        private HIVCohortDefinitionLibrary hivCohortDefinitionLibrary;
-        @Autowired
-        private ARTCohortLibrary  artCohortLibrary;
+
         /**
          * @return the uuid for the report design for exporting to Excel
          */
         @Override
         public String getExcelDesignUuid() {
-                return "051ca7a6-ae2c-4fac-8495-77300d25b947";
+                return "34213c70-36bd-4e7a-8e76-9ded9e8c8397";
         }
+
         @Override
         public String getUuid() {
                 return "6325ccbd-2d98-4e8c-bc1c-befc2eb0dfa5";
         }
+
         @Override
         public String getName() {
-                return " Lost Clients";
+                return "LOST CLIENTS";
         }
+
         @Override
         public String getDescription() {
-                return "Lost Clients";
+                return "LOST CLIENTS";
         }
+
         @Override
         public List<Parameter> getParameters() {
                 List<Parameter> l = new ArrayList<Parameter>();
@@ -70,12 +79,14 @@ public class SetUpLostReport extends UgandaEMRDataExportManager {
                 l.add(df.getEndDateParameter());
                 return l;
         }
+
         @Override
         public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
                 List<ReportDesign> l = new ArrayList<ReportDesign>();
                 l.add(buildReportDesign(reportDefinition));
                 return l;
         }
+
         /**
          * Build the report design for the specified report, this allows a user to override the report design by adding
          * properties and other metadata to the report design
@@ -84,29 +95,36 @@ public class SetUpLostReport extends UgandaEMRDataExportManager {
          * @return The report design
          */
         @Override
+
         public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
-                ReportDesign rd = createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "LostClients.xls");
+                ReportDesign rd = createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "Lost.xls");
                 Properties props = new Properties();
-                props.put("repeatingSections", "sheet:1,row:10,dataset:LOST");
+                props.put("repeatingSections", "sheet:1,row:7,dataset:LOST_CLIENTS");
                 props.put("sortWeight", "5000");
                 rd.setProperties(props);
                 return rd;
         }
+
         @Override
         public ReportDefinition constructReportDefinition() {
+
                 ReportDefinition rd = new ReportDefinition();
+
                 rd.setUuid(getUuid());
                 rd.setName(getName());
                 rd.setDescription(getDescription());
                 rd.setParameters(getParameters());
+
                 PatientDataSetDefinition dsd = new PatientDataSetDefinition();
-                CohortDefinition clientsLost = df.getLostClients();
+
+                CohortDefinition definition = df.getLostClients();
+
                 dsd.setName(getName());
                 dsd.setParameters(getParameters());
-                dsd.addRowFilter(Mapped.mapStraightThrough(clientsLost));
-                dsd.addColumn( "Patient Name",  new PreferredNameDataDefinition(), (String) null);
-                dsd.addColumn( "Sex", new GenderDataDefinition(), (String) null);
-                dsd.addColumn("Birth Date", builtInPatientData.getBirthdate(), "", new BirthDateConverter());
+                dsd.addRowFilter(Mapped.mapStraightThrough(definition));
+                dsd.addColumn("Patient Name", new PreferredNameDataDefinition(), (String) null);
+                dsd.addColumn("Sex", new GenderDataDefinition(), (String) null);
+                dsd.addColumn("Birth Date", new BirthdateDataDefinition(), (String) null);
                 addColumn(dsd, "Age", builtInPatientData.getAgeAtStart());
                 addColumn(dsd, "Telephone", basePatientData.getTelephone());
                 addColumn(dsd, "HIV Enrolled Date", hivPatientData.getEnrollmentDate());
@@ -114,11 +132,13 @@ public class SetUpLostReport extends UgandaEMRDataExportManager {
                 addColumn(dsd, "Current Regimen",hivPatientData.getCurrentRegimen());
                 addColumn(dsd, "Last Visit Date",hivPatientData.getLastVisitDate());
                 addColumn(dsd, "Last Appointment",hivPatientData.getExpectedReturnDate());
-                addColumn(dsd, "Date Seen", hivPatientData.getLastARTEncounter());
-                rd.addDataSetDefinition("LOST_CLIENT", Mapped.mapStraightThrough(dsd));
-                rd.setBaseCohortDefinition(Mapped.mapStraightThrough(clientsLost));
+
+                rd.addDataSetDefinition("LOST_CLIENTS", Mapped.mapStraightThrough(dsd));
+                rd.setBaseCohortDefinition(Mapped.mapStraightThrough(definition));
+
                 return rd;
         }
+
         @Override
         public String getVersion() {
                 return "0.1";
