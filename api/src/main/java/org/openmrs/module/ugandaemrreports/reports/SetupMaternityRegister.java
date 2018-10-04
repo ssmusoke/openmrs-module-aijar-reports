22f058590d5dceff8c1439e5d018a64ba204dd5d
solemabrothers <ssevvume1994solema@gmail.com>
2018-10-04 11:40:50 +0300

    Maternity Register Exportable as CSV

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupMaternityRegister.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupMaternityRegister.java
@@ -36,12 +36,12 @@ public class SetupMaternityRegister extends UgandaEMRDataExportManager {
 	
 	@Override
 	public String getUuid() {
-		return "3607d6cb-e0cd-4296-8b4e-cd2ec8fd190c";
+		return "640cfa10-c96b-4a37-837f-77cffe08d771";
 	}
 	
 	@Override
 	public String getVersion() {
-		return "0.1";
+		return "1.0";
 	}
 	
 	/**
@@ -49,7 +49,7 @@ public class SetupMaternityRegister extends UgandaEMRDataExportManager {
      */
 	@Override
 	public String getExcelDesignUuid() {
-		return "19cb4e89-cc1a-49ce-82aa-8c5982a00599";
+		return "898f734e-f635-422d-af73-ff2298c4a682";
 	}
 	
 	@Override
@@ -58,10 +58,12 @@ public class SetupMaternityRegister extends UgandaEMRDataExportManager {
 		l.add(df.getStartDateParameter());
 		return l;
 	}
+
 	
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		List<ReportDesign> l = new ArrayList<ReportDesign>();
+		l.add(buildExcel(reportDefinition));
 		l.add(buildReportDesign(reportDefinition));
 		return l;
 	}
@@ -73,9 +75,15 @@ public class SetupMaternityRegister extends UgandaEMRDataExportManager {
 	 * @param reportDefinition
 	 * @return The report design
 	 */
-	@Override	
+	@Override
 	public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
-		ReportDesign rd = createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "MaternityRegister.xls");
+		ReportDesign rd = createCSVDesign(getExcelDesignUuid(), reportDefinition);
+		return rd;
+	}
+
+
+	public ReportDesign buildExcel(ReportDefinition reportDefinition) {
+		ReportDesign rd = createExcelTemplateDesign("23ad46d2-83ab-4b38-b913-489c4d880460", reportDefinition, "MaternityRegister.xls");
 		Properties props = new Properties();
 		props.put("repeatingSections", "sheet:1,row:8,dataset:Maternity");
 		props.put("sortWeight", "5000");
