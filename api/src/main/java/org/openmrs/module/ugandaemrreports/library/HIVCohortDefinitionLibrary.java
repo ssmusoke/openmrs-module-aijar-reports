0d044494a98d902d4499f82e7c1ca64ca79af844
balukujr <juniorderrick18@gmail.com>
2019-02-04 14:56:58 +0300

    Completed cohorts for over due for viral load and computed next viral load date

--- api/src/main/java/org/openmrs/module/ugandaemrreports/library/HIVCohortDefinitionLibrary.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/library/HIVCohortDefinitionLibrary.java
@@ -395,4 +395,20 @@ public class HIVCohortDefinitionLibrary extends BaseDefinitionLibrary<CohortDefi
     public CohortDefinition getActiveWithNoEncounterInQuarter() {
         return df.getActiveInPeriodWithoutVisit();
     }
+
+    public CohortDefinition getPatientsWhoseLastViralLoadWasMonthsAgoFromEndDate(String monthsBack){
+        return df.getPatientsWhoseObsValueDateIsByEndDate(hivMetadata.getViralLoadDate(),
+                Arrays.asList(hivMetadata.getARTEncounterEncounterType()),BaseObsCohortDefinition.TimeModifier.LAST,monthsBack);
+    }
+
+    public CohortDefinition getPatientsWithLastViralLoadByEndDate(){
+        return df.getPatientsWhoseObsValueDateIsByEndDate(hivMetadata.getViralLoadDate(),
+                Arrays.asList(hivMetadata.getARTEncounterEncounterType()),BaseObsCohortDefinition.TimeModifier.ANY
+        );
+    }
+
+    public CohortDefinition getPatientsWithArtStartDateByEndDate(String olderThan) {
+        return df.getPatientsWhoseObsValueDateIsByEndDate(hivMetadata.getArtStartDate(), hivMetadata.getARTSummaryPageEncounterType(), BaseObsCohortDefinition.TimeModifier.ANY, olderThan);
+    }
+
 }
