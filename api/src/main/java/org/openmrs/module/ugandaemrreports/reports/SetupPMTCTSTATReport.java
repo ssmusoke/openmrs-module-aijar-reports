1f54840500d2fb3b71dfb9fecab1de4467cf87c9
balukujr <juniorderrick18@gmail.com>
2019-02-12 16:19:27 +0300

    Set up for PMTCT STAT Report

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupPMTCTSTATReport.java
@@ -0,0 +1,184 @@
+package org.openmrs.module.ugandaemrreports.reports;
+
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
+import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.indicator.CohortIndicator;
+import org.openmrs.module.reporting.indicator.dimension.CohortDefinitionDimension;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.ugandaemrreports.library.*;
+import org.openmrs.module.ugandaemrreports.metadata.HIVMetadata;
+import org.openmrs.module.ugandaemrreports.reporting.utils.ReportUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *  PMTCT STAT Report
+ */
+@Component
+public class SetupPMTCTSTATReport extends UgandaEMRDataExportManager {
+
+    @Autowired
+    private DataFactory df;
+
+    @Autowired
+    ARTClinicCohortDefinitionLibrary hivCohorts;
+
+    @Autowired
+    private BuiltInPatientDataLibrary builtInPatientData;
+
+    @Autowired
+    private HIVPatientDataLibrary hivPatientData;
+
+    @Autowired
+    private BasePatientDataLibrary basePatientData;
+
+    @Autowired
+    private HIVMetadata hivMetadata;
+
+    @Autowired
+    private HIVCohortDefinitionLibrary hivCohortDefinitionLibrary;
+
+    @Autowired
+    private CommonCohortDefinitionLibrary commonCohortDefinitionLibrary;
+
+    @Autowired
+    private CommonDimensionLibrary commonDimensionLibrary;
+
+    @Autowired
+    private Moh105IndicatorLibrary indicatorLibrary;
+
+
+    /**
+     * @return the uuid for the report design for exporting to Excel
+     */
+    @Override
+    public String getExcelDesignUuid() {
+        return "880cf4b5-07eb-4951-b13d-c77701daef5c";
+    }
+
+    @Override
+    public String getUuid() {
+        return "2356ee4a-c974-40e3-b28d-5b56f76ce295";
+    }
+
+    @Override
+    public String getName() {
+        return "PMTCT STAT Report";
+    }
+
+    @Override
+    public String getDescription() {
+        return "PMTCT STAT Report";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(df.getStartDateParameter());
+        l.add(df.getEndDateParameter());
+        return l;
+    }
+
+
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        return Arrays.asList(buildReportDesign(reportDefinition));
+    }
+
+    /**
+     * Build the report design for the specified report, this allows a user to override the report design by adding
+     * properties and other metadata to the report design
+     *
+     * @param reportDefinition
+     * @return The report design
+     */
+    @Override
+    public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
+        return createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "MER_PMTCT_STAT.xls");
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+        String params = "startDate=${startDate},endDate=${endDate}";
+
+        ReportDefinition rd = new ReportDefinition();
+
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+
+        CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+
+        dsd.setParameters(getParameters());
+        rd.addDataSetDefinition("PMTCT_STAT", Mapped.mapStraightThrough(dsd));
+
+        CohortDefinitionDimension ageDimension =commonDimensionLibrary.getPMTCT_STAT_AgeGenderGroup();
+        dsd.addDimension("age", Mapped.mapStraightThrough(ageDimension));
+
+        addGender(dsd,"a","Total  Pregnant With known HIV status at entry",ReportUtils.map(indicatorLibrary.pregnantTrkTrrk(),params),"female"); ;
+        addGender(dsd,"b","Total  Pregnant Known HIV positives at entry",ReportUtils.map(indicatorLibrary.pregnantTrrk(),params),"female");
+        addGender(dsd,"c","Pregnant Total Newly tested for HIV",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRAndTRR(),params),"female");
+        addGender(dsd,"d","Total  newly Pregnant Tested  HIV Positive",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRR(),params),"female");
+        addGender(dsd,"e","Total  Number of NEW ANC Clients ",ReportUtils.map(indicatorLibrary.anc1stVisit()),"female");
+        addGender(dsd,"f","Total  Male that tested For HIV",ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params),"male");
+        addGender(dsd,"g","Total  Male that tested HIV +ve",ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultHivPositive(), params),"male");
+
+        add105AgeGenderDimensions(dsd,"h","Pregnant newly and tested for HIV",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRAndTRR(), params));
+        add105AgeGenderDimensions(dsd,"i","Pregnant newly and tested HIV +ve for 1st time",ReportUtils.map(indicatorLibrary.pregnantWomenNewlyTestedForHivThisPregnancyTRR(), params));
+        add105AgeGenderDimensions(dsd,"j","1st ANC Visit",ReportUtils.map(indicatorLibrary.anc1stVisit(), params));
+
+        addIndicator(dsd, "4h" , "Total pregnant who knew HIV status before 1st ANC" , ReportUtils.map(indicatorLibrary.pregnantTrkTrrk(), params), "");
+        addIndicator(dsd, "5h" , "Total pregnant who were HIV +ve before 1st ANC" , ReportUtils.map(indicatorLibrary.pregnantTrrk(), params), "");
+        addIndicator(dsd, "9h" ,"Pregnant Women testing HIV+ on a retest " , ReportUtils.map(indicatorLibrary.retestedTrrPlus(), params),"");
+        addIndicator(dsd, "13h" ,"Total Males received HIV test results" , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params),"");
+        addIndicator(dsd, "14h" ,"Males received HIV test +ve " , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultHivPositive(), params),"");
+
+        addIndicator(dsd, "2f" , "Total  Male that tested For HIV" , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params), "age=below10male");
+        addIndicator(dsd, "3f" , "Total  Male that tested For HIV" , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params), "age=below10male");
+        addIndicator(dsd, "4f" , "Total  Male that tested For HIV" , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params), "age=below10male");
+        addIndicator(dsd, "5f" , "Total  Male that tested For HIV" , ReportUtils.map(indicatorLibrary.malePatinersRecievedHivResultTotal(), params), "age=below10male");
+
+        return rd;
+    }
+
+
+    public void addGender (CohortIndicatorDataSetDefinition dsd,String key ,String label,Mapped<? extends CohortIndicator> cohortIndicator , String gender){
+        addIndicator(dsd, "1" + key, label , cohortIndicator, "age=below10"+gender);
+        addIndicator(dsd, "2" + key, label , cohortIndicator, "age=between10and14"+gender);
+        addIndicator(dsd, "3" + key, label , cohortIndicator, "age=between15and19"+gender);
+        addIndicator(dsd, "4" + key, label , cohortIndicator, "age=between20and24"+gender);
+        addIndicator(dsd, "5" + key, label , cohortIndicator, "age=between25and29"+gender);
+        addIndicator(dsd, "6" + key, label , cohortIndicator, "age=between30and34"+gender);
+        addIndicator(dsd, "7" + key, label , cohortIndicator, "age=between35and39"+gender);
+        addIndicator(dsd, "8" + key, label , cohortIndicator, "age=between40and49"+gender);
+        addIndicator(dsd, "9" + key, label , cohortIndicator, "age=above50"+gender);
+    }
+
+    public void add105AgeGenderDimensions(CohortIndicatorDataSetDefinition dsd,String key ,String label,Mapped<? extends CohortIndicator> cohortIndicator){
+        addIndicator(dsd, "1" + key, label , cohortIndicator, "age=between10and19female");
+        addIndicator(dsd, "2" + key, label , cohortIndicator, "age=between20and24female");
+        addIndicator(dsd, "3" + key, label , cohortIndicator, "age=25+female");
+    }
+
+
+    public void addIndicator(CohortIndicatorDataSetDefinition dsd, String key, String label, Mapped<? extends CohortIndicator> ci, String dimensionOptions) {
+        dsd.addColumn(key, label, ci, dimensionOptions);
+    }
+
+
+    @Override
+    public String getVersion() {
+        return "0.1";
+    }
+}
