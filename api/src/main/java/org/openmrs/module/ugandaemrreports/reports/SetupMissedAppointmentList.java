2d7b16b552541a0bfc4cd8a0f6e378a5df5a0f83
solemabrothers <solemasolomon@gmail.com>
2019-03-15 14:28:05 +0300

    Add Addresses, contact numbers, Enrollment and ART Start dates to Missed Appointment Report

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupMissedAppointmentList.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupMissedAppointmentList.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.ugandaemrreports.reports;
 
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.data.converter.BirthdateConverter;
 import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
 import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
@@ -123,7 +124,14 @@ public class SetupMissedAppointmentList extends UgandaEMRDataExportManager {
         addColumn(dsd, "EID No", hivPatientData.getEIDNumber());
         dsd.addColumn("Patient Name", new PreferredNameDataDefinition(), (String) null);
         dsd.addColumn("Sex", new GenderDataDefinition(), (String) null);
-        dsd.addColumn("Birth Date", new BirthdateDataDefinition(), (String) null);
+        dsd.addColumn("Birth Date", new BirthdateDataDefinition(), "", new BirthdateConverter("MMM dd,yyyy"));
+        addColumn(dsd, "Telephone", basePatientData.getTelephone());
+        addColumn(dsd, "Age", builtInPatientData.getAgeAtStart());
+        addColumn(dsd, "Enrollment Date", hivPatientData.getSummaryPageDate());
+        addColumn(dsd, "Art Start Date", hivPatientData.getARTStartDate());
+        addColumn(dsd,"Parish",df.getPreferredAddress("address4"));
+        addColumn(dsd,"Village",df.getPreferredAddress("address5"));
+        addColumn(dsd,"Directions",hivPatientData.getDirectionsToPatientAddress());
         addColumn(dsd, "Supposed Visit Date", hivPatientData.getExpectedReturnDateDuringPeriod());
         addColumn(dsd, "Date Seen", hivPatientData.getLastARTEncounter());
 
@@ -135,6 +143,6 @@ public class SetupMissedAppointmentList extends UgandaEMRDataExportManager {
 
     @Override
     public String getVersion() {
-        return "0.2";
+        return "2.0.2";
     }
 }
