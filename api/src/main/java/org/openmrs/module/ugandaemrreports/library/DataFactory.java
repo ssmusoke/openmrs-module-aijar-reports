ee42e846cc25ded773d953c8adfd87a2b41c852d
balukujr <juniorderrick18@gmail.com>
2019-04-15 12:41:53 +0300

    Renamed Cohort from the merging files that that had been renamed

--- api/src/main/java/org/openmrs/module/ugandaemrreports/library/DataFactory.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/library/DataFactory.java
@@ -1104,7 +1104,7 @@ public class DataFactory {
      * @return
      */
     public CohortDefinition getPatientsWhoHaveNotComeAfterTheirLastMissedAppointmentByMinimumDays(int minimumDays){
-        LostPatientsCohortDefinition cd = new LostPatientsCohortDefinition();
+        PatientsWhoDidntTurnupForScheduledAppointmentCohortDefinition cd = new PatientsWhoDidntTurnupForScheduledAppointmentCohortDefinition();
         cd.setMinimumDays(minimumDays);
         cd.addParameter(new Parameter("startDate", "startDate", Date.class));
         cd.addParameter(new Parameter("endDate", "Ending", Date.class));
