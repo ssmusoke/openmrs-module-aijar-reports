79afc33e7684a8660477e9c3ad2279214202087a
solemabrothers <ssevvume1994solema@gmail.com>
2019-04-15 09:18:25 +0300

    DSDM data Export report indicating the Current Patient Model And Date of Enrollment

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/common/DSDMModel.java
@@ -0,0 +1,42 @@
+package org.openmrs.module.ugandaemrreports.common;
+
+import org.openmrs.module.reporting.common.DateUtil;
+
+import java.util.Date;
+
+/**
+ */
+public class DSDMModel {
+    private Date dateOfEnrollment;
+    private String progId;
+
+    public DSDMModel(Date dateOfEnrollment) {
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
+
+    public String toString() {
+        return DateUtil.formatDate(dateOfEnrollment, "MMM d, yyyy", "");
+    }
+
+    public DSDMModel(Date dateOfEnrollment, String progId) {
+        this.dateOfEnrollment = dateOfEnrollment;
+        this.progId = progId;
+    }
+
+    public Date getdateOfEnrollment() {
+        return dateOfEnrollment;
+    }
+
+    public void setdateOfEnrollment(Date dateOfEnrollment) {
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
+
+    public String getprogId() {
+        return progId;
+    }
+
+    public void setprogId(String programId) {
+        this.progId = programId;
+    }
+
+}
