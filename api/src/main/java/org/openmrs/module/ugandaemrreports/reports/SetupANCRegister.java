0470d4dff5a1bc26831bc5b562ada6a5d008e1c8
solemabrothers <ssevvume1994solema@gmail.com>
2019-05-09 12:00:27 +0300

    Add EMTCT codes, foric and Iron given  to the ANC register

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupANCRegister.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupANCRegister.java
@@ -131,7 +131,7 @@ public class SetupANCRegister extends UgandaEMRDataExportManager {
 
     @Override
     public String getVersion() {
-        return "0.2";
+        return "1.0.8";
     }
 
     @Override
