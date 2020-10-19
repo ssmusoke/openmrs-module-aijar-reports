335e93b82da4deb540cc93fa18d63d73c33fb1a3
balukujr <juniorderrick18@gmail.com>
2020-10-19 12:14:50 +0300

    updated Transferred Out cohort to get only patients transferred out during the period

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/Setup106A1A2019Report.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/Setup106A1A2019Report.java
@@ -162,7 +162,7 @@ public class Setup106A1A2019Report extends UgandaEMRDataExportManager {
 
         CohortDefinition everEnrolledByEndQuarter = df.getPatientsNotIn(enrolledBeforeQuarter, enrolledInTheQuarter);
         CohortDefinition enrolledDuringTheQuarter = df.getPatientsNotIn(enrolledInTheQuarter, transferredInTheQuarter);
-        CohortDefinition transferredOutPatients = hivCohortDefinitionLibrary.getPatientsTransferredOutByEndOfPeriod();
+        CohortDefinition transferredOutPatients = hivCohortDefinitionLibrary.getPatientsTransferredOutBetweenStartAndEndDate();
         CohortDefinition deadPatientsDuringPeriod =df.getPatientsInAll(everEnrolledByEndQuarter, df.getDeadPatientsDuringPeriod());
         CohortDefinition lost_to_followup = df.getLostToFollowUp();
 
