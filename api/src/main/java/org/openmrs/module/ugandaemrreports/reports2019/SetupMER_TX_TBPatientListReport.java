14c7f75f9c2feb68b86f68cab84a05357920da7f
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-04 10:25:58 +0300

    Add templates for TX_TB and TB_ART patient list reports

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports2019/SetupMER_TX_TBPatientListReport.java
@@ -0,0 +1,172 @@
+package org.openmrs.module.ugandaemrreports.reports2019;
+
+
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.data.converter.BirthdateConverter;
+import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
+import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
+import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.ugandaemrreports.library.ARTClinicCohortDefinitionLibrary;
+import org.openmrs.module.ugandaemrreports.library.BasePatientDataLibrary;
+import org.openmrs.module.ugandaemrreports.library.DataFactory;
+import org.openmrs.module.ugandaemrreports.library.HIVCohortDefinitionLibrary;
+import org.openmrs.module.ugandaemrreports.library.HIVPatientDataLibrary;
+import org.openmrs.module.ugandaemrreports.reports.UgandaEMRDataExportManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *  TX TB Patient Listing Report
+ */
+@Component
+public class SetupMER_TX_TBPatientListReport extends UgandaEMRDataExportManager {
+
+    @Autowired
+    private DataFactory df;
+
+    @Autowired
+    ARTClinicCohortDefinitionLibrary hivCohorts;
+    @Autowired
+    private HIVCohortDefinitionLibrary hivCohortDefinitionLibrary;
+
+    @Autowired
+    private BuiltInPatientDataLibrary builtInPatientData;
+
+    @Autowired
+    private HIVPatientDataLibrary hivPatientData;
+
+    @Autowired
+    private BasePatientDataLibrary basePatientData;
+
+    /**
+     * @return the uuid for the report design for exporting to Excel
+     */
+    @Override
+    public String getExcelDesignUuid() {
+        return "134f5bed-4544-4bba-a784-cec683ac0be1";
+    }
+
+    public String getCSVDesignUuid() {
+        return "5be40c97-0f87-48ff-8445-af576b0442c1";
+    }
+
+    @Override
+    public String getUuid() {
+        return "9a98f32f-c9e5-42d8-bfff-81ede6161ea4";
+    }
+
+    @Override
+    public String getName() {
+        return "Patient List for Tx_TB MER Indicator Report";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Tx_TB Patient List Report";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(df.getStartDateParameter());
+        l.add(df.getEndDateParameter());
+        return l;
+    }
+
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        List<ReportDesign> l = new ArrayList<ReportDesign>();
+        l.add(buildReportDesign(reportDefinition));
+        l.add(buildCSVReportDesign(reportDefinition));
+        return l;
+    }
+
+    /**
+     * Build the report design for the specified report, this allows a user to override the report design by adding
+     * properties and other metadata to the report design
+     *
+     * @param reportDefinition
+     * @return The report design
+     */
+    @Override
+    public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
+        ReportDesign rd = createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "MER_TX_TB_PatientList.xls");
+        Properties props = new Properties();
+        props.put("repeatingSections", "sheet:1,row:8,dataset:TX_TB_PatientList");
+        props.put("sortWeight", "5000");
+        rd.setProperties(props);
+        return rd;
+    }
+
+    public ReportDesign buildCSVReportDesign(ReportDefinition reportDefinition) {
+        return createCSVDesign(getCSVDesignUuid(), reportDefinition);
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+
+        ReportDefinition rd = new ReportDefinition();
+
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+
+        PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+
+        dsd.setParameters(getParameters());
+        rd.addDataSetDefinition("TX_TB_PatientList", Mapped.mapStraightThrough(dsd));
+
+        CohortDefinition returnToCareClients= df.getPatientsInAll(
+                hivCohortDefinitionLibrary.getActivePatientsWithLostToFollowUpAsByDays("28"),
+                hivCohortDefinitionLibrary.getPatientsWithNoClinicalContactsForAbove28DaysByBeginningOfPeriod()
+        );
+
+        dsd.setName(getName());
+        dsd.setParameters(getParameters());
+        dsd.addRowFilter(Mapped.mapStraightThrough(returnToCareClients));
+
+        addColumn(dsd, "Clinic No", hivPatientData.getClinicNumber());
+        addColumn(dsd, "EID No", hivPatientData.getEIDNumber());
+        dsd.addColumn("Patient Name", new PreferredNameDataDefinition(), (String) null);
+        dsd.addColumn("Sex", new GenderDataDefinition(), (String) null);
+        dsd.addColumn("Birth Date", new BirthdateDataDefinition(), "", new BirthdateConverter("MMM dd,yyyy"));
+        addColumn(dsd, "Telephone", basePatientData.getTelephone());
+        addColumn(dsd, "Age", builtInPatientData.getAgeAtStart());
+        addColumn(dsd, "Enrollment Date", hivPatientData.getSummaryPageDate());
+        addColumn(dsd, "Art Start Date", hivPatientData.getARTStartDate());
+        addColumn(dsd,"Parish",df.getPreferredAddress("address4"));
+        addColumn(dsd,"Village",df.getPreferredAddress("address5"));
+        addColumn(dsd, "ART Start Date", hivPatientData.getArtStartDate());
+        addColumn(dsd, "Current Regimen", hivPatientData.getCurrentRegimen());
+        addColumn(dsd, "VL Quantitative",  hivPatientData.getCurrentViralLoad());
+        addColumn(dsd, "VL Date", hivPatientData.getViralLoadDate());
+        addColumn(dsd,"VL Qualitative",hivPatientData.getVLQualitativeByEndDate());
+        addColumn(dsd, "VL Quantitative",  hivPatientData.getCurrentViralLoad());
+        addColumn(dsd,"Directions",hivPatientData.getDirectionsToPatientAddress());
+        addColumn(dsd, "Last Visit Date", hivPatientData.getLastARTVisitEncounterByEndOfPreviousPeriod(df.getEncounterDatetimeConverter()));
+        addColumn(dsd, "Expected Return Date", hivPatientData.getLatestExpectedReturnDateBeforeStartDate());
+        addColumn(dsd, "RTT Visit Date", hivPatientData.getLastEncounterByEndDate());
+
+        rd.addDataSetDefinition("TX_TB_PatientList", Mapped.mapStraightThrough(dsd));
+        rd.setBaseCohortDefinition(Mapped.mapStraightThrough(returnToCareClients));
+
+        return rd;
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.0.2";
+    }
+}
