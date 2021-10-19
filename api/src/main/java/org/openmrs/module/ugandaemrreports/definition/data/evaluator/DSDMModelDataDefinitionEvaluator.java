699aac8d63a31eca8c81e326912d08fa42076ed4
dbaluku <juniorderrick18@gmail.com>
2021-10-19 14:17:56 +0300

    Excluding HIV Program and other programs from DSDM Columns in facility reports (#283)

--- api/src/main/java/org/openmrs/module/ugandaemrreports/definition/data/evaluator/DSDMModelDataDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/definition/data/evaluator/DSDMModelDataDefinitionEvaluator.java
@@ -58,7 +58,11 @@ public class DSDMModelDataDefinitionEvaluator implements PatientDataEvaluator {
         String query = "Select pg.patient_id, p.name,pg.date_enrolled,pg.date_completed\n" +
                 "             from patient_program as pg\n" +
                 "             Inner join program p on p.program_id=pg.program_id\n" +
-                "where ((pg.date_completed > '"+endDate+"') OR date_completed IS NULL)  group by pg.patient_id";
+                "where ((pg.date_completed > '"+endDate+"') OR date_completed IS NULL) and p.uuid in ('de5d54ae-c304-11e8-9ad0-529269fb1459',\n" +
+                "    'de5d5b34-c304-11e8-9ad0-529269fb1459',\n" +
+                "    'de5d5896-c304-11e8-9ad0-529269fb1459',\n" +
+                "    'de5d5da0-c304-11e8-9ad0-529269fb1459',\n" +
+                "    'de5d6034-c304-11e8-9ad0-529269fb1459')  group by pg.patient_id";
 
 
         SqlQueryBuilder q = new SqlQueryBuilder(query);
