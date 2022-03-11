8b964457f05bb2dc510e5a220e777cdfc87ca482
balukujr <juniorderrick18@gmail.com>
2022-03-11 09:49:14 +0300

    Setup SMS Report

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupSMSAppointmentsReports.java
@@ -0,0 +1,158 @@
+package org.openmrs.module.ugandaemrreports.reports;
+
+import org.openmrs.Program;
+import org.openmrs.module.reporting.cohort.definition.BaseObsCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.common.ObjectUtil;
+import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
+import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
+import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.ugandaemrreports.definition.cohort.definition.CurrentlyInProgramCohortDefinition;
+import org.openmrs.module.ugandaemrreports.definition.data.converter.BirthDateConverter;
+import org.openmrs.module.ugandaemrreports.definition.dataset.definition.UgandaEMRMobileDatasetDefinition;
+import org.openmrs.module.ugandaemrreports.library.*;
+import org.openmrs.module.ugandaemrreports.metadata.HIVMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * SMS Reports with people on appointment in a particular period
+ */
+@Component
+public class SetupSMSAppointmentsReports extends UgandaEMRDataExportManager {
+
+    @Autowired
+    private DataFactory df;
+
+    @Autowired
+    ARTClinicCohortDefinitionLibrary hivCohorts;
+
+    @Autowired
+    private BuiltInPatientDataLibrary builtInPatientData;
+
+    @Autowired
+    private HIVPatientDataLibrary hivPatientData;
+
+    @Autowired
+    private BasePatientDataLibrary basePatientData;
+
+    @Autowired
+    private HIVMetadata hivMetadata;
+
+    @Autowired
+    private HIVCohortDefinitionLibrary hivCohortDefinitionLibrary;
+
+
+    @Override
+    public String getExcelDesignUuid() {
+        return "20f4c992-f657-4952-af51-9d71d3205901";
+    }
+
+    public String getCSVDesignUuid() {
+        return "e8cd7d9f-e30e-463f-8073-ea7ccf3d3574";
+    }
+
+    @Override
+    public String getUuid() {
+        return "9a4bfceb-6205-4811-9a09-f95589249f65";
+    }
+
+    @Override
+    public String getName() {
+        return "SMS Reminder Appointments List";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Lists Patients to Send SMS With Appointments in a particular period";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(df.getStartDateParameter());
+        l.add(df.getEndDateParameter());
+        l.add(new Parameter("programs", "programs", Program.class, List.class, null,null,false));
+        return l;
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        List<ReportDesign> l = new ArrayList<ReportDesign>();
+        l.add(buildReportDesign(reportDefinition));
+        return l;
+    }
+
+    @Override
+    public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
+        return createCSVDesign(getCSVDesignUuid(), reportDefinition);
+
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+
+        ReportDefinition rd = new ReportDefinition();
+
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+
+        PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+
+        CohortDefinition appointmentList = df.getPatientsWhoseObsValueDateIsBetweenStartDateAndEndDate(hivMetadata.getReturnVisitDate(), Arrays.asList(hivMetadata.getARTEncounterEncounterType()), BaseObsCohortDefinition.TimeModifier.ANY);
+        CohortDefinition patientsDeadAndTransferredOut = hivCohortDefinitionLibrary.getDeadAndTransferredOutPatientsDuringPeriod();
+        CohortDefinition hadEncounterInPeriod = hivCohortDefinitionLibrary.getArtPatientsWithEncounterOrSummaryPagesBetweenDates();
+        CohortDefinition cohortstoExclude =df.getPatientsInAny(hadEncounterInPeriod,patientsDeadAndTransferredOut);
+        CohortDefinition patientsWithAppointments=df.getPatientsNotIn(appointmentList,cohortstoExclude);
+
+        CurrentlyInProgramCohortDefinition cd = new CurrentlyInProgramCohortDefinition();
+        cd.setName("in program during period");
+        cd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+        cd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
+        cd.addParameter(new Parameter("programs", "programs", Program.class));
+        CohortDefinition inSMSProgram = df.convert(cd, ObjectUtil.toMap("onOrAfter=endDate,onOrBefore=endDate,programs=programs"));
+
+        CohortDefinition all = df.getPatientsInAll(inSMSProgram,patientsWithAppointments);
+
+        dsd.setName(getName());
+        dsd.setParameters(getParameters());
+        dsd.addRowFilter(Mapped.mapStraightThrough(all));
+        addColumn(dsd,"Person UUID",hivPatientData.getPatientUUID());
+        addColumn(dsd, "Clinic No", hivPatientData.getClinicNumber());
+        addColumn(dsd, "EID No", hivPatientData.getEIDNumber());
+        dsd.addColumn("Patient Name", new PreferredNameDataDefinition(), (String) null);
+        addColumn(dsd, "Sex", builtInPatientData.getGender());
+        dsd.addColumn("Birth Date", builtInPatientData.getBirthdate(), "", new BirthDateConverter());
+        addColumn(dsd,"Parish",df.getPreferredAddress("address4"));
+        addColumn(dsd,"Village",df.getPreferredAddress("address5"));
+        addColumn(dsd, "ART Start Date", hivPatientData.getArtStartDate());
+        addColumn(dsd, "Current Regimen", hivPatientData.getCurrentRegimen());
+        addColumn(dsd, "Current Regimen UUID", hivPatientData.getCurrentRegimenUuid() );
+        addColumn(dsd, "VL Date", hivPatientData.getViralLoadDate());
+        addColumn(dsd, "VL Quantitative",  hivPatientData.getCurrentViralLoad());
+        addColumn(dsd,"VL Qualitative",hivPatientData.getVLQualitativeByEndDate());
+        addColumn(dsd,"DSDM Model", hivPatientData.getDSDMModel());
+        addColumn(dsd,"DSDM Model Enrollment Date",   hivPatientData.getDSDMEnrollmentDate());
+        addColumn(dsd, "Appointment Date", hivPatientData.getExpectedReturnDateBetween());
+        addColumn(dsd, "Telephone", basePatientData.getTelephone());
+
+        rd.addDataSetDefinition("APPOINTMENT_LIST", Mapped.mapStraightThrough(dsd));
+        return rd;
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
