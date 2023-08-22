3b4805493ecbe31553e6230fddd9cd1ae955302a
dbaluku <juniorderrick18@gmail.com>
2023-08-22 22:27:13 +0300

    evaluate report definition and return report data

---
+++ omod/src/main/java/org/openmrs/module/ugandaemrreports/web/resources/EvaluateReportDefinitionRestController.java
@@ -0,0 +1,133 @@
+package org.openmrs.module.ugandaemrreports.web.resources;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.service.CohortDefinitionService;
+import org.openmrs.module.reporting.common.DateUtil;
+import org.openmrs.module.reporting.common.ReflectionUtil;
+import org.openmrs.module.reporting.dataset.DataSet;
+import org.openmrs.module.reporting.dataset.DataSetRow;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.report.ReportData;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.definition.service.ReportDefinitionService;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@Controller
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + EvaluateDataSetRestController.UGANDAEMRREPORTS + EvaluateReportDefinitionRestController.SET)
+public class EvaluateReportDefinitionRestController {
+    public static final String UGANDAEMRREPORTS = "/ugandaemrreports";
+    public static final String SET = "/reportingDefinition";
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public Object getReportData(@RequestParam String startDate, @RequestParam String endDate,
+                                @RequestParam(required = false, value = "uuid") String reportDefinitionUuid) {
+        try {
+            if (!validateDateIsValidFormat(endDate)) {
+                SimpleObject message = new SimpleObject();
+                message.put("error", "given date " + endDate + "is not valid");
+                return new ResponseEntity<SimpleObject>(message, HttpStatus.BAD_REQUEST);
+
+            }
+            EvaluationContext context = new EvaluationContext();
+
+            ReportDefinitionService service = Context.getService(ReportDefinitionService.class);
+            ReportDefinition rd = service.getDefinitionByUuid(reportDefinitionUuid);
+            ReportData reportData = null;
+            if (rd != null) {
+                Map<String, Object> parameterValues = new HashMap<String, Object>();
+
+                parameterValues.put("endDate", DateUtil.parseYmd(endDate));
+                parameterValues.put("startDate", DateUtil.parseYmd(startDate));
+
+                context.addParameterValue("endDate", DateUtil.parseYmd(endDate));
+                context.addParameterValue("startDate", DateUtil.parseYmd(startDate));
+
+                reportData = getReportDefinitionService().evaluate(rd, context);
+
+            }
+
+            Map<String, List<SimpleObject>> listMap = new HashMap<>();
+            Map<String, DataSet> dataSets = reportData.getDataSets();
+            Set<String> keySet = dataSets.keySet();
+            for (String key : keySet) {
+                    DataSet dataSet = dataSets.get(key);
+                List<SimpleObject> simpleObjectList=  convertDataSetToSimpleObject(dataSet);
+                listMap.put(key, simpleObjectList);
+            }
+
+            return new ResponseEntity<Map<String, List<SimpleObject>>>(listMap, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<String>(ex.getMessage() + Arrays.toString(ex.getStackTrace()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public Boolean validateDateIsValidFormat(String date) {
+        try {
+            DateUtil.parseYmd(date);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public List<SimpleObject> getTraceReportData(DataSet d) {
+        Iterator iterator = d.iterator();
+
+        List<SimpleObject> dataList = new ArrayList<SimpleObject>();
+        while (iterator.hasNext()) {
+            DataSetRow r = (DataSetRow) iterator.next();
+            Map<String, Object> columns = r.getColumnValuesByKey();
+            Set<String> keys = columns.keySet();
+            SimpleObject details = new SimpleObject();
+
+            for (String key : keys) {
+                details.add(key, r.getColumnValue(key));
+            }
+            dataList.add(details);
+
+        }
+        return dataList;
+    }
+
+    private ReportDefinitionService getReportDefinitionService() {
+        return Context.getService(ReportDefinitionService.class);
+    }
+
+    public List<SimpleObject> convertDataSetToSimpleObject(DataSet d) {
+        Iterator iterator = d.iterator();
+
+        List<SimpleObject> dataList = new ArrayList<SimpleObject>();
+        while (iterator.hasNext()) {
+            DataSetRow r = (DataSetRow) iterator.next();
+            Map<String, Object> columns = r.getColumnValuesByKey();
+            Set<String> keys = columns.keySet();
+            SimpleObject details = new SimpleObject();
+
+            for (String key : keys) {
+                try{
+                    details.add(key, (String) r.getColumnValue(key));
+
+                }catch (Exception e ){
+                    e.printStackTrace();
+                }
+            }
+            dataList.add(details);
+
+        }
+        return dataList;
+    }
+}
