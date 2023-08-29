36f6b089bb49bbec2f1144cf1dfab1797c4da375
dbaluku <juniorderrick18@gmail.com>
2023-08-29 19:11:14 +0300

    Added functionality to save and retrieve and DashboardReportObject

--- api/src/main/java/org/openmrs/module/ugandaemrreports/api/UgandaEMRReportsService.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/api/UgandaEMRReportsService.java
@@ -1,7 +1,11 @@
 package org.openmrs.module.ugandaemrreports.api;
 
+import org.openmrs.api.APIException;
 import org.openmrs.api.OpenmrsService;
+import org.openmrs.module.ugandaemrreports.model.DashboardReportObject;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * This service exposes module's core functionality. It is a Spring managed bean which is configured in
@@ -16,5 +20,36 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Transactional
 public interface UgandaEMRReportsService extends OpenmrsService {
-	
+
+    /**
+     * Getting all Report objects for dashboard
+     *
+     * @return List<DashboardReportObject> returns all report objects in a list
+     * @throws APIException
+     */
+    List<DashboardReportObject> getAllDashboardReportObjects() throws APIException;
+
+    /**
+     * Get Report object  By uuid
+     *
+     * @param uuid the uuid of the report object  to return
+     * @return Dashboard report object that matched the uuid parameter
+     * @throws APIException
+     */
+    @Transactional
+    DashboardReportObject getDashboardReportObjectByUUID(String uuid) throws APIException;
+
+    /**
+     * Saves the Dashboard Report Object
+     *
+     * @param dashboardReportObject to be saved.
+     * @return DashboardReportObject saved
+     * @throws APIException
+     */
+    @Transactional
+    DashboardReportObject saveDashboardReportObject(DashboardReportObject dashboardReportObject) throws APIException;
+
+    @Transactional
+    DashboardReportObject getDashboardReportObjectById(Integer id) throws APIException;
+
 }
--- api/src/main/java/org/openmrs/module/ugandaemrreports/api/impl/UgandaEMRReportsServiceImpl.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/api/impl/UgandaEMRReportsServiceImpl.java
@@ -2,30 +2,59 @@ package org.openmrs.module.ugandaemrreports.api.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.APIException;
 import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.ugandaemrreports.api.UgandaEMRReportsService;
 import org.openmrs.module.ugandaemrreports.api.db.UgandaEMRReportsDAO;
+import org.openmrs.module.ugandaemrreports.api.db.hibernate.HibernateUgandaEMRReportsDAO;
+import org.openmrs.module.ugandaemrreports.model.DashboardReportObject;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * It is a default implementation of {@link UgandaEMRReportsService}.
  */
+
+
 public class UgandaEMRReportsServiceImpl extends BaseOpenmrsService implements UgandaEMRReportsService {
 
 	protected final Log log = LogFactory.getLog(this.getClass());
 
-	private UgandaEMRReportsDAO dao;
+	private HibernateUgandaEMRReportsDAO dao;
 
 	/**
 	 * @return the dao
 	 */
-	public UgandaEMRReportsDAO getDao() {
+	public HibernateUgandaEMRReportsDAO getDao() {
 		return dao;
 	}
 
 	/**
 	 * @param dao the dao to set
 	 */
-	public void setDao(UgandaEMRReportsDAO dao) {
+	public void setDao(HibernateUgandaEMRReportsDAO dao) {
 		this.dao = dao;
+	}
+
+
+	@Override
+	public List<DashboardReportObject> getAllDashboardReportObjects() throws APIException {
+		return  dao.getAllDashboardReportObjects();
+	}
+
+	@Override
+	public DashboardReportObject getDashboardReportObjectByUUID(String uuid) throws APIException {
+		return dao.getDashboardReportObjectByUUID(uuid);
+	}
+
+	@Override
+	public DashboardReportObject saveDashboardReportObject(DashboardReportObject dashboardReportObject) throws APIException {
+		return dao.saveDashboardReportObject(dashboardReportObject);
+	}
+
+	@Override
+	public DashboardReportObject getDashboardReportObjectById(Integer id) throws APIException {
+		return  dao. getDashboardReportObjectById( id);
 	}
 }
