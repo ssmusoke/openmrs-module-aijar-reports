1b916e49f208b152bf3e14a8b59f8015506f926d
dbaluku <juniorderrick18@gmail.com>
2023-11-27 10:10:15 +0300

    Fixing dates conversion on data set definitions (#346)
    * converted dates to localdatetime
    
    * Adding ability to return html for aggregate HIE reports

--- api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupTxCurrent_28Days2019Report.java
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reports/SetupTxCurrent_28Days2019Report.java
@@ -198,6 +198,6 @@ public class SetupTxCurrent_28Days2019Report extends UgandaEMRDataExportManager 
 
     @Override
     public String getVersion() {
-        return "0.4.7";
+        return "0.5.1";
     }
 }
