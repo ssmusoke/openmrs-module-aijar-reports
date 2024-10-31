6917a8b485bb60d3d93cb01cd50a4d0c2317efab
dbaluku <juniorderrick18@gmail.com>
2024-10-31 15:22:02 +0300

    U4X-174:HMIS 105 Report in 4x (#362)
    U4X-174:HMIS 105 Report in 4x (#362)

---
+++ omod/src/main/resources/_etl/derived/hts/sp_data_processing_derived_hts.sql
@@ -0,0 +1,4 @@
+-- $BEGIN
+CALL sp_fact_encounter_hts_card;
+
+-- $END
--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -7,4 +7,5 @@ CALL sp_data_processing_derived_transfers();
 CALL sp_data_processing_derived_non_suppressed();
 CALL sp_data_processing_derived_hiv_art_card();
 CALL sp_data_processing_derived_IIT();
+CALL sp_data_processing_derived_hts();
     -- $END
